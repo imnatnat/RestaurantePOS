8174ce188a6686ca3a0d326d6f5977991ec3c3ce
Nat.Nat <78292634+imnatnat@users.noreply.github.com>
2026-04-23 17:12:22 -0600

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,23 @@
 document.getElementById("loginForm")?.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const res = await fetch("https://restaurant-pos-backend-i88u.onrender.com", {
+
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+
+  const res = await fetch("https://restaurant-pos-backend-i88u.onrender.com/login", {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({
-      username: user.value,
-      password: pass.value
+      username: user,
+      password: pass
     })
   });
+
   const data = await res.json();
-  if(data.success){
+
+  if (data.success) {
     window.location.href = "dashboard.html";
   } else {
     alert("Login failed");
